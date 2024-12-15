89517ccff3f2223f974dc9f5f74ec74b75f1d507
doukn0wdaway <anisimov.sasha20@gmail.com>
2024-12-15 17:26:57 +0200

    fixes

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ import { loadChatIds } from "./fs.js";
 import { onDisconnect, onReconnect, pingServiceSetup } from "./pingService.js";
 
 pingServiceSetup(
-  async () => {
-    console.log("onDisconnect");
-    loadChatIds().forEach((el) => {
-      bot.telegram.sendMessage(el.id, onDisconnect(), {
+  () => {
+    console.log("on disconnect");
+    loadChatIds().forEach(async (el) => {
+      bot.telegram.sendMessage(el.id, await onDisconnect(), {
         parse_mode: "HTML",
       });
     });
